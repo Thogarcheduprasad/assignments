a54d69b20606995a44cd4155d2495404c1fe799f
prasad <tprasad2244@gmail.com>
2022-05-25 21:22:42 +0530

    hol3

---
+++ Day3Mvc/Day3Mvc/Day3Mvc/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.Entity;
+namespace Day3Mvc.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+                 : base("name=conn")
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
