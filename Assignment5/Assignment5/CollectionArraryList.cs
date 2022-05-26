7cda61961bbc00ac714d63c693ef04221fd7cbd8
prasad <tprasad2244@gmail.com>
2022-05-26 08:34:01 +0530

    assignment5

---
+++ Assignment5/Assignment5/CollectionArraryList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace Assignment5
+{
+
+    class CollectionArrayList
+    {
+        public static void Main()
+        {
+            ArrayList al = new ArrayList();
+            al.Add("balaram");
+            al.Add("sumanth");
+            al.Add("umesh");
+            al.Add("pavan");
+            al.Add("sharath");
+            al.Add("sameer");
+            al.Add("uday");
+            al.Add("prasad");
+            al.Add(547);
+            Console.WriteLine(al.Capacity);
+
+            foreach (object o in al)
+            {
+                Console.WriteLine(o);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Assignment5/Assignment5/StackOperation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    internal class Sample
+    {
+
+
+        public static void Main()
+        {
+            Stack<String> ss = new Stack<string>();
+            ss.push("Prasad");
+            ss.push("subbu");
+            ss.push("venkatesh");
+            Console.WriteLine(ss.pop());
+            Console.WriteLine(ss.pop());
+            Console.WriteLine(ss.pop());
+            Console.WriteLine(ss.pop());
+            Console.ReadKey();
+
+        }
+    }
+    public class Stack<T>
+    {
+        public int size;
+        public StackElement<T> top;
+        public Stack()
+        {
+            size = 0;
+            top = null;
+        }
+        public void push(T newvalue)
+        {
+            StackElement<T> newElement = new StackElement<T>(newvalue, top);
+            top = newElement;
+            size++;
+        }
+
+        public T pop()
+        {
+            StackElement<T> oldTop = top;
+
+            if (size == 0)
+            {
+                return default(T);
+                Console.WriteLine("Stack is Empty");
+            }
+            //try
+            //{
+            top = top.getNext();
+            return oldTop.getValue();
+            //}
+            //catch(Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+        }
+
+    }
+    public class StackElement<T>
+    {
+        public T value;
+        public StackElement<T> next;
+
+        public StackElement(T value, StackElement<T> next)
+        {
+            this.value = value;
+            this.next = next;
+        }
+
+        public StackElement<T> getNext()
+        {
+            return next;
+        }
+        public T getValue()
+        {
+            return value;
+        }
+
+
+    }
+}
