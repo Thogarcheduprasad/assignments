23e2b426c15c2ce173344b024eb4b3e29963799a
prasad <tprasad2244@gmail.com>
2022-05-26 08:32:46 +0530

    assignment3

---
+++ Assignment3/Assignment3/Mystack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Mystack
+    {
+        static readonly int max = 100;
+        int top1;
+        int[] st=new int[max];
+
+
+        public void Push(int data)
+        {
+            if (top1 > max)
+            {
+                Console.WriteLine("Stack Overflow");
+
+            }
+            else
+            {
+                top1 = max;
+
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Mystack ms=new Mystack();
+                ms.Push(10);
+                ms.Push(20);
+                ms.Push(30);
+
+
+
+               
+
+            }
+        }
+
+    }
+}
